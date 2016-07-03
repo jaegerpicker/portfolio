1a60998b2543e459b0ff6b9f12d33aed52bd0467
Shawn Campbell <scampbell@vetsfirstchoice.com>
2016-07-03 16:51:17 -0400

    Switched to gulp added testing, still failing, and cleaned up a few things too big of a commit

---
+++ tasks-gulp/register/test.js
@@ -0,0 +1,18 @@
+module.exports = function(gulp, plugins) {
+  gulp.task('pre-test', function() {
+    return gulp.src(['lib/**/*.js'])
+      .pipe(plugins.istanbul())
+      .pipe(plugins.istanbul.hookRequire());
+  });
+
+  gulp.task('test', ['pre-test'], function(cb) {
+    return gulp.src(['./test/**/*.js'])
+      .pipe(plugins.mocha({
+        reporter: 'mocha-junit-reporter',
+        reporterOptions: {
+          mochaFile: process.env.CIRCLE_TEST_REPORTS + '/junit/results.xml'
+        }
+      }))
+      .pipe(plugins.istanbul.writeReports());
+  });
+};
